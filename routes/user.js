614e4b27a4178e264554015c744f6f70fbbee5cb
josuecorrea <josuejtc@gmail.com>
2016-08-26 00:04:50 -0300

    profile

--- routes/user.js
+++ routes/user.js
@@ -33,6 +33,7 @@ router.post('/signup', function (req, res, next) {
        user.profile.name = req.body.name;
        user.password = req.body.password;
        user.email = req.body.email;
+       user.profile.picture = user.gravatar();
 
        User.findOne({email: req.body.email}, function (err, existingUser) {
 
@@ -42,7 +43,12 @@ router.post('/signup', function (req, res, next) {
             }else{
                 user.save(function(err,user){
                     if(err) return next(err);
-                    return res.redirect('/');
+                    //return res.redirect('/');
+
+                    req.logIn(user, function(err){
+                        if(err) return next(err);
+                        res.redirect('/profile');
+                    });
                 });
             }
          });
@@ -50,6 +56,7 @@ router.post('/signup', function (req, res, next) {
 
 router.get('/logout', function (req, res, next) {
     req.logout();
+    res.redirect('/');
   });
 
 module.exports = router;
