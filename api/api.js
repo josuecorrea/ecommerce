d5bec8cdc43f41077ee08a2d025d645454bcdd60
josuecorrea <josuejtc@gmail.com>
2016-08-26 17:08:22 -0300

    faker api

--- api/api.js
+++ api/api.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-var router = require('async');
+var async = require('async');
 var faker = require('faker');
 var Category = require('../models/category');
 var Product = require('../models/product');
@@ -7,22 +7,26 @@ var Product = require('../models/product');
 router.get('/:name', function(req, res, next){
     async.waterfall([
         function(callback){
-            Category.findOne({name: req.body.name}, function (err, category) {
+            Category.findOne({name: req.params.name}, function (err, category) {
                 if(err) return next(err);
                 callback(null, category);
               });
         },
-        function (callback) {
+        function (category, callback) {
             for(var i = 0; i < 30; i++){
                 var product = new Product();
 
                 product.category = category._id;
                 product.name = faker.commerce.productName();
                 product.price = faker.commerce.price();
-                product.image = faker.commerce.image();
+                product.image = faker.image.image();
 
                 product.save();
             }
           }
-    ])
+    ]);
+
+    res.json({message: 'Success'});
 });
+
+module.exports = router;
