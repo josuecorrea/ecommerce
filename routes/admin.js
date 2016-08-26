c63d3bc620b09b34c70d982c2160f020d986d981
josuecorrea <josuejtc@gmail.com>
2016-08-26 16:01:59 -0300

    category

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+var router = require('express').Router();
+var Category = require ('../models/category.js');
+
+
+router.get('/add-category', function(req, res, next){
+    res.render('admin/add-category', {message: req.flash('success')});
+});
+
+router.post('/add-category', function(req, res, next){
+    var category = new Category();
+
+    category.name = req.body.name;
+
+    category.save(function(err){
+        if(err) next(err);
+        req.flash('success', 'Sucessfully added a category');
+        return res.redirect('/add-category');
+    });   
+});
+
+module.exports = router;
