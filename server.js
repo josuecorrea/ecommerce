748edb993821ab2c5d87ef8a964e4be499cd46f3
josuecorrea <josuejtc@gmail.com>
2016-08-26 15:01:49 -0300

    profile

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var flash = require('express-flash');
 var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
 
-
 var secret = require('./config/secret');
 var User = require('./models/user');
 
